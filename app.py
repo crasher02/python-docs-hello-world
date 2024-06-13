0b2ae9a24a3f720cd25fe3ebf1885cac8299e015
crasher02 <79001406+crasher02@users.noreply.github.com>
2024-06-13 16:14:42 +0200

    Deployment2 app.py

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from flask import Flask
+from datetime import datetime
+import pytz
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Hackerman! This is your Pre-Production Environment"
-
-from datetime import datetime
-import pytz
-
-def main():
-    # Display "Hello World!"
-    print("Hello World!")
-    
     # Get the current date and time in Zurich
     tz_zurich = pytz.timezone('Europe/Zurich')
     zurich_time = datetime.now(tz_zurich)
@@ -19,8 +13,8 @@ def main():
     # Format the date and time as a string
     zurich_time_str = zurich_time.strftime('%Y-%m-%d %H:%M:%S %Z%z')
     
-    # Display the date and time
-    print(f"Current date and time in Zurich: {zurich_time_str}")
+    # Return the message with the current date and time
+    return f"Hello, Hackerman! This is your Pre-Production Environment. Current date and time in Zurich: {zurich_time_str}"
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
